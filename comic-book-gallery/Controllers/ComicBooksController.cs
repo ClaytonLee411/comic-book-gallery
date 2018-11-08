1d9d4232f52ebe9f4037ded4ccea61084e817b13
Clayton <clayton.lee411@gmail.com>
2018-11-07 22:28:23 -0500

    Initial commit

---
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comic_book_gallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        // GET: ComicBooks
+        public ActionResult Detail ()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books control"
+
+            };
+        }
+    }
+}
